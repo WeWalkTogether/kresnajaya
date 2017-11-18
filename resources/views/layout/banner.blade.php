1884c4f3c50e5d75dde488b4a93fb20c5a725389
unknown <kreskiani23@gmail.com>
2017-11-18 13:50:21 +0700

    memasukan web kedalam folder

---
+++ resources/views/layout/banner.blade.php
@@ -0,0 +1,23 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<div data-vide-bg="video/video">
+    <div class="container">
+		<div class="banner-info">
+			<h3>It is a long established fact that a reader will be distracted by 
+			the readable </h3>	
+			<div class="search-form">
+				<form action="#" method="post">
+					<input type="text" placeholder="Search..." name="Search...">
+					<input type="submit" value=" " >
+				</form>
+			</div>		
+		</div>	
+    </div>
+</div>
+
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
+<script src="js/jquery.vide.min.js"></script>
